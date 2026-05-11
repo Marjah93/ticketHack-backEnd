4cbbfdaa2f8f40773bd4f187c8f81c268e651bf1
rciron <ciron.romain@gmail.com>
2026-05-11 21:23:52 +0200

    add MVC structure router and database connection

---
+++ routes/bookings.js
@@ -0,0 +1,10 @@
+// TO DO create the routes for the bookings
+const express = require('express');
+const router = express.Router();
+const { Booking } = require('../models/booking');
+
+// Create a new booking via POST
+router.post('/', async (req, res) => {
+    // TODO: add new code here
+    }
+);
